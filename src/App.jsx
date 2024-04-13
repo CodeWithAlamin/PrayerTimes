0ddfba29154ff66281cf863955aeefb814d93974
alamin <codepapa360@gmail.com>
2024-04-14 00:08:08 +0600

    getting data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
+import { callApi } from "./Services";
+import { useState } from "react";
+
 function App() {
-  return <div>Hi</div>;
+  const [timings, setTimings] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await callApi();
+      const { timings } = data;
+
+      setTimings(timings);
+    }
+
+    fetchData();
+  }, []);
+
+  const array = Object.entries(timings);
+
+  return (
+    <div>
+      {array.map((time) => (
+        <p key={time}>
+          {time[0]} : {time[1]}
+        </p>
+      ))}{" "}
+    </div>
+  );
 }
 
 export default App;
--- src/Services.js
+++ src/Services.js
@@ -1,3 +1,8 @@
-function callApi() {
-  const url = "https://jsonplaceholder.typicode.com/posts";
+export async function callApi() {
+  const url =
+    "https://api.aladhan.com/v1/timingsByCity/13-04-2024?city=Dhaka&country=Bangladesh&method=8";
+
+  const response = await fetch(url).then((response) => response.json());
+
+  return response;
 }
