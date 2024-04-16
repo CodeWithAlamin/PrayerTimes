b0f391caf441c284e56856e4775808b171e18866
Alamin <codepapa360@gmail.com>
2024-04-16 11:58:46 +0600

    Remove unnecessary codes

--- src/common.js
+++ src/common.js
@@ -1,7 +1,4 @@
 export function formateTime(time) {
-  // sample time 2024-04-14T19:24:00+06:00
-  // to nice format in 12 hours 07:24 PM
-
   const date = new Date(time);
   let hour = date.getHours();
   const minute = date.getMinutes();
@@ -11,7 +8,7 @@ export function formateTime(time) {
     ampm = "PM";
   }
 
-  // pad 0 if hour or minute is less than 10
+  // Add 0 if hour or minute is less than 10
   const hourStr = hour < 10 ? `0${hour}` : hour;
   const minuteStr = minute < 10 ? `0${minute}` : minute;
 
